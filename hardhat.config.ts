b4de6961c11f2d49958cd6593228f99f5a19b415
Chris <crgold@asu.edu>
2022-12-12 00:23:05 +0800

    update hardhat config to use a node

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+
+const {API_URL_KEY, PRIVATE_KEY} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: API_URL_KEY,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
 
 export default config;
